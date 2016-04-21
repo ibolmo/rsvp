930a6bdf8114f7bee704588dbaf44e974c80694f
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-04-21 17:10:56 -0500

    simple php server to explain

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$URL = 'https://script.google.com/macros/s/AKfycbz2rN-aAQG8GquFxdcRtmg_IuNQpdyp4LLQEAxEH-nQXzI_cL1q/exec';
+
+$json = json_decode(file_get_contents($URL), true);
+$config = $json['config'];
+
+if (preg_match('/\/rsvp/i', @$_SERVER['REQUEST_URI'])){
+	header("Location: {$config['facebook.event']}");
+	die;
+}
