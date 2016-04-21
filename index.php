8d398ed43a6d0b843903cd2d6c432b815adb5c87
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-04-21 17:11:19 -0500

    obligatory todo

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $URL = 'https://script.google.com/macros/s/AKfycbz2rN-aAQG8GquFxdcRtmg_IuNQpdyp4LLQEAxEH-nQXzI_cL1q/exec';
 
+# todo(ibolmo): use caching
 $json = json_decode(file_get_contents($URL), true);
 $config = $json['config'];
 
