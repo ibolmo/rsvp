bed267cbf7b7c9ade9f40ef7fcd5de83cbc27838
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-04-21 17:21:02 -0500

    more robust

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # rsvp
 Simple PHP server to demonstrate how Code RGV's always right URL works
+
+# Getting Started
+
+> # run server
+> ./server
+> 
+> # test server
+> curl -L http://localhost:8000             # no response
+> curl -L http://localhost:8000/rsvp        # should redirect
+
+Modify `index.php` based off of the [Spreadsheet]() and the [Google Apps Script]()
+
+
+# Self-Update Mechanism
+You can add a `onEdit` callback on your spreadsheet that calls your `/update` on your server.
+
+> # test self-update
+> curl -L http://localhost:8000/update
--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
-$URL = 'https://script.google.com/macros/s/AKfycbz2rN-aAQG8GquFxdcRtmg_IuNQpdyp4LLQEAxEH-nQXzI_cL1q/exec';
-
-# todo(ibolmo): use caching
-$json = json_decode(file_get_contents($URL), true);
-$config = $json['config'];
-
 if (preg_match('/\/rsvp/i', @$_SERVER['REQUEST_URI'])){
+	if (!file_exists('./config.json')){
+		echo 'Need to update!';
+		die;
+	}
+
+	$json = json_decode(file_get_contents('./config.json'), true);
+	$config = $json['config'];
 	header("Location: {$config['facebook.event']}");
+	die;
+}
+
+if (preg_match('/\/update/i', @$_SERVER['REQUEST_URI'])){
+	$URL = 'https://script.google.com/macros/s/AKfycbz2rN-aAQG8GquFxdcRtmg_IuNQpdyp4LLQEAxEH-nQXzI_cL1q/exec';
+	$json = file_get_contents($URL);
+	file_put_contents('./config.json', $json);
 	die;
 }
